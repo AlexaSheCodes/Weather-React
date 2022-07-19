d76466aab744d3ba0b7eea8893004ecec3eacf53
AlexaSheCodes <101257665+AlexaSheCodes@users.noreply.github.com>
2022-07-19 13:20:15 +0300

    some changes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,19 +5,20 @@ import "./WeatherForecast.css";
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast" id="forecast">
-      <div className="center" />
       <div className="row">
         <div className="col">
-          <div className="WeatherForecast-day">
-            Monday
-            <div />
-            <p>
-              <WeatherIcon code="01d" />
-              <div className="WeatherForecast-temperatures">
-                <span className="WeatherForecast-temperature-max">35 /</span>
-                <span className="WeatherForecast-temperature-min">15</span>
-              </div>
-            </p>
+          <div className="center">
+            <div className="WeatherForecast-day">
+              Monday
+              <div />
+              <p>
+                <WeatherIcon code="01d" size={32} />
+                <div className="WeatherForecast-temperatures">
+                  <span className="WeatherForecast-temperature-max">35° </span>
+                  <span className="WeatherForecast-temperature-min">15°</span>
+                </div>
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,7 +8,7 @@ export default function WeatherInfo(props) {
     <div className="WeatherInfo">
       <h1 id="h1">{props.data.city}</h1>
       <div className="float-left">
-        <WeatherIcon id="icon" code={props.data.icon} />
+        <WeatherIcon id="props.icon" code={props.data.icon} size={95} />
       </div>
       <WeatherTemperature celsius={props.data.temperature} />
       <ul>
